ccfb94dde307f61f0149214fd7ff80f3fea2beef
yung00se <johnmkmosley@gmail.com>
2024-09-13 21:00:32 -0500

    Added "Room" functionality

--- BlazorBattleship/Hubs/ChatHub.cs
+++ BlazorBattleship/Hubs/ChatHub.cs
@@ -3,15 +3,46 @@ namespace BlazorBattleship.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendCoordinates(string user, int x, int y)
+        //private static int[] roomCounts = { 0, 0, 0, 0, 0 };
+        private static List<List<string>> playersInRooms = new List<List<string>>();
+
+        public async Task SendReadyMessage(int room)
         {
-            await Clients.Others.SendAsync("ReceiveCoordinates", user, x, y);
+            await Clients.OthersInGroup($"Room {room}").SendAsync("ReceiveReadyMessage");
         }
 
-        public async Task SendHitMiss(bool wasHit)
+        public async Task UpdateRooms()
         {
-            await Clients.Others.SendAsync("ShotResponse", wasHit);
+            for (int i = 0; i < 5; i++) {
+                playersInRooms.Add(new List<string>());
+            }
+            await Clients.All.SendAsync("ReceiveRooms", playersInRooms);
         }
 
+        public async Task JoinRoom(string user, int room)
+        {
+            while (playersInRooms.Count != 5)
+            {
+                playersInRooms.Add(new List<string>());
+            }
+
+            if (playersInRooms[room-1].Count < 2)
+            {
+                playersInRooms[room-1].Add(user);
+            }
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, $"Room {room}");
+            await Clients.All.SendAsync("ReceiveRooms", playersInRooms);
+        }
+
+        public async Task SendCoordinates(string user, int room, int x, int y)
+        {
+            await Clients.OthersInGroup($"Room {room}").SendAsync("ReceiveCoordinates", user, x, y);
+        }
+
+        public async Task SendHitMiss(int room, bool wasHit)
+        {
+            await Clients.OthersInGroup($"Room {room}").SendAsync("ShotResponse", wasHit);
+        }
     }
 }
