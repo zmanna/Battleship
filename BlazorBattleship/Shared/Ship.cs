0fa1af502bc67101948dc3c03ed18a6ff0de008f
PDykes1996 <pdykes1996@gmail.com>
2024-09-15 00:45:07 -0500

    Fixing Branch
    Overriding old test branch with current main.

---
+++ BlazorBattleship/Shared/Ship.cs
@@ -0,0 +1,36 @@
+﻿namespace BlazorBattleship.Shared
+{
+    public class Ship
+    {
+        public List<(int x, int y)> shipCoords;
+        public int length;
+        public int hp;
+        public string name;
+
+        public Ship(int length, int hp, string name, List<(int x, int y)> coords)
+        {
+            this.length = length;
+            this.hp = hp;
+            this.name = name;
+            this.shipCoords = coords;
+        }
+
+        public bool IsDestroyed()
+        {
+            return hp <= 0;
+        }
+
+        public bool IsHit(int x, int y)
+        {
+            return shipCoords.Contains((x, y));
+        }
+
+        public void DeductHP()
+        {
+            if (hp > 0)
+            {
+                this.hp--;
+            }
+        }
+    }
+}
